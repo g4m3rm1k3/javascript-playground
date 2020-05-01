2af3c0894b22d1f1548b7cb3c8cdccae34df6b82
Michael McLean <g4m3rm1k3@hotmail.com>
2020-05-01 04:43:17 -0400

    use of square brackets with objects

--- playground.js
+++ playground.js
@@ -1,1 +1,17 @@
-let data = {a:1,b:2,c:3,}
+const numbers = {
+  100: 'one hundred',
+  16 : 'sixteen',
+  '76 trombones' : 'great song'
+};
+console.log(numbers[100])
+console.log(numbers['76 trombones'])
+
+const palette ={
+  red: '#eb4d4b',
+  yellow: '#f9ca24',
+  blue: '#30336b'
+}
+
+let mysteryColor = 'yellow';
+console.log(palette[mysteryColor]);
+console.log(palette['bl' + 'ue']);
