bbb36e29e15fa1ba0a378a52c48afe4d4fc6d2d4
Michael McLean <g4m3rm1k3@hotmail.com>
2020-05-01 04:25:04 -0400

    object where number is not a valid key

--- playground.js
+++ playground.js
@@ -1,1 +1,8 @@
-alert("Hello World")
+const fitBitData = {
+  totalSteps : 308727,
+  totalMiles : 211.7,
+  avgCalorieBurn: 5755,
+  workoutsThisWeek : '5 of 7',
+  avgGoodSleep: '2:13',
+  45 : 'fory five'
+};
