e27220304ea77de0a38fd1e2db327afa75af1822
Michael McLean <g4m3rm1k3@hotmail.com>
2020-05-01 04:27:35 -0400

    end comma is optional

--- playground.js
+++ playground.js
@@ -1,8 +1,1 @@
-const fitBitData = {
-  totalSteps : 308727,
-  totalMiles : 211.7,
-  avgCalorieBurn: 5755,
-  workoutsThisWeek : '5 of 7',
-  avgGoodSleep: '2:13',
-  45 : 'fory five'
-};
+let data = {a:1,b:2,c:3,}
