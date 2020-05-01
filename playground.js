42f3a372dd3dd6e97791db3b5f2c6ea11843fd32
Michael McLean <g4m3rm1k3@hotmail.com>
2020-05-01 05:25:03 -0400

    nested objects

--- playground.js
+++ playground.js
@@ -1,13 +1,11 @@
-const userReviews = {};
-
-userReviews['queenBee49'] = 4.0;
-userReviews.mrSmith78 = 3.5;
-
-console.log(userReviews);
-userReviews.colt = '5';
-console.log(userReviews);
-userReviews.colt = 5;
-console.log(userReviews);
-userReviews['queenBee49'] += 2;
-userReviews.mrSmith78 ++;
-console.log(userReviews);
+const student = {
+  fistName : "Michael",
+  lastName : "McLean",
+  strengths : ['Maths, Arts'],
+  exams: {
+    midterm: 92,
+    final: 88
+  },
+}
+const average = (student.exams.midterm + student.exams.final)/2
+console.log(average);
